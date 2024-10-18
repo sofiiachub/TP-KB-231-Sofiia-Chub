901f263d06193779f54a3438d3d49936a720962e
sofiiachub <chub@stu.cn.ua>
2024-10-18 10:57:31 +0300

    Create task2.py

---
+++ topic_03/task2.py
@@ -0,0 +1,23 @@
+childrenList = ['Stacey', 'Thomas', 'Newt', 'Jery'] 
+print(childrenList) 
+
+childrenList.extend(['Ilona', 'Nata']) 
+print(childrenList) 
+
+childrenList.append('Jusel') 
+print(childrenList) 
+
+childrenList.insert(5, 'Rita') 
+print(childrenList) 
+
+childrenList.remove('Stacey') 
+print(childrenList) 
+
+childrenList.sort() 
+print(childrenList) 
+
+childrenList.reverse() 
+print(childrenList) 
+
+childrenListCopy = childrenList.copy() 
+print(childrenListCopy) 
